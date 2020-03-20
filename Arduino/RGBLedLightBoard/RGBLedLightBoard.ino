07a5ffe31c5c86a9f1af4495156a1b8e961b4950
Christopher Kuzma <5973601+ckuzma@users.noreply.github.com>
2020-03-19 17:05:23 -0700

    First visualization

--- Arduino/RGBLedLightBoard/RGBLedLightBoard.ino
+++ Arduino/RGBLedLightBoard/RGBLedLightBoard.ino
@@ -1,1 +1,70 @@
-// This is nothing
+#include <Adafruit_NeoMatrix.h>
+
+#define PIN D3
+#define WIDTH 16
+#define HEIGHT 8
+
+int displayArray[HEIGHT][WIDTH][3] = {{0, 0, 0}};
+
+Adafruit_NeoMatrix Matrix = Adafruit_NeoMatrix(
+  HEIGHT, WIDTH,
+  PIN,
+  NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_ROWS   + NEO_MATRIX_ZIGZAG,
+  NEO_GRB           + NEO_KHZ800
+  );
+  
+void setup() {
+  // Initialize the matrix
+  Matrix.begin();
+
+  // Initialize the random seed
+  randomSeed(2020);
+}
+
+void displayBoard() {
+  for(int x = 0; x < HEIGHT; x++){
+    for(int y = 0; y < WIDTH; y++) {
+      int r = displayArray[x][y][0];
+      int g = displayArray[x][y][1];
+      int b = displayArray[x][y][2];
+        Matrix.drawPixel(x, y, Matrix.Color(r, g, b));
+    }
+  }
+  Matrix.show();
+}
+
+void changePixel(int x, int y, int r, int g, int b) {
+  displayArray[x][y][0] = r;
+  displayArray[x][y][1] = g;
+  displayArray[x][y][2] = b;
+}
+
+void makePrettyColors() {
+  // Our selected "pretty color" options
+  int prettyColors[5][3] = { // [number_of_colors][r,g,b]
+    {30,0,0},
+    {0,20,0},
+    {0,0,15},
+    {30,20,0},
+    {0,30,20}
+  };
+
+  // Pick a random pixel to change
+  int x = random(HEIGHT);
+  int y = random(WIDTH);
+
+  // Pick a color and change the pixel
+  int colorNumber = random(5);
+  changePixel(x, y, prettyColors[colorNumber][0], prettyColors[colorNumber][1], prettyColors[colorNumber][2]);
+}
+
+void loop() {
+  // Simple animation
+  makePrettyColors();
+
+  // Display it!
+  displayBoard();
+
+  delay(80);
+}
--- README.md
+++ README.md
@@ -1,17 +1,1 @@
 # rgb-led-light-board
-
-## Credit
-- _GreatScottLab_'s wonderful Instructables tutorial which provided the initial code I used to test my array and borrowed some ideas from
-    - [Instructables: Make Your Own 10x10 LED Matrix](https://www.instructables.com/id/Make-Your-Own-10x10-LED-Matrix/)
-    - [SinzianaPanel](https://github.com/ckuzma/SinzianaPanel) code repo, a copy of SinzianaPanel.rar found on the above Instructable
-
-## Requirements
-- Adafruit NeoPixel Library
-    - https://github.com/adafruit/Adafruit_NeoPixel
-    - v1.3.5 or greater
-- Adafruit NeoMatrix Library
-    - https://github.com/adafruit/Adafruit_NeoMatrix
-    - v1.1.4 or greater
-- Adafruit GFX Library
-    - https://github.com/adafruit/Adafruit-GFX-Library
-    - v1.7.5 or greater
