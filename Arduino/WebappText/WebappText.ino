9db65bfec44203368a47ca63607929165456cf9d
ckuzma <5973601+ckuzma@users.noreply.github.com>
2020-06-12 13:40:35 -0700

    Cleaned up comments, indenting, removed extra libraries

--- Arduino/WebappText/WebappText.ino
+++ Arduino/WebappText/WebappText.ino
@@ -2,61 +2,57 @@
 // https://www.youtube.com/watch?v=TfzFJkDgg7s
 
 #include <ESP8266WiFi.h>
-#include "espneotext.h"
-#include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
-#include <Adafruit_NeoPixel.h>
-#ifndef PSTR
- #define PSTR // Make Arduino Due happy
-#endif
+#include "TextPageHTML.h"
 
+// Setup for our particular display
 #define PIN D3
 #define WIDTH 16
 #define HEIGHT 8
 
-const char* ssid = "WIFI_2.4GHZ_SSID";
+// WiFi settings and text color
+const char* ssid     = "WIFI_2.4GHZ_SSID";
 const char* password = "WIFI_PASSWORD";
+const int red   = 255;
+const int green = 255;
+const int blue  = 255;
+
+// Place to store the string
 String value;
-//char charBuf[60];
-int z=0;
+
+// Used later for calculating scroll lengths
 int pixelsInText;
-// Create an instance of the server
-// specify the port to listen on as an argument
+int z=0;
+
+// Start the server and listen on port 80
 WiFiServer server(80);
 
-
-
-// Array initialization
+// Setup the array
 Adafruit_NeoMatrix Matrix = Adafruit_NeoMatrix(
-  WIDTH, HEIGHT,
-  PIN,
+  WIDTH, HEIGHT, PIN,
   NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
   NEO_MATRIX_ROWS   + NEO_MATRIX_ZIGZAG,
   NEO_GRB           + NEO_KHZ800
-  );
+);
 
-const uint16_t colors[] = {
-  Matrix.Color(255, 255, 255),
-  Matrix.Color(0, 255, 0),
-  Matrix.Color(0, 0, 255) };
-
+// Not sure
+int x = Matrix.width();
 
 void setup() {
-    Matrix.begin();
+  Serial.begin(115200);
+
+  // Initialize the matrix
+  Matrix.begin();
   Matrix.setTextWrap(false);
   Matrix.setBrightness(255);
-  Matrix.setTextColor(colors[0]);
-  Serial.begin(115200);
-  delay(10);
+  Matrix.setTextColor(Matrix.Color(red, green, blue));
 
-    // Connect to WiFi network
+  // Connect to WiFi
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
   WiFi.begin(ssid, password);
-  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -64,20 +60,15 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
   
-  // Start the server
+  // Start server
   server.begin();
   Serial.println("Server started");
 
-  // Print the IP address
+  // Print serverIP address
   Serial.println(WiFi.localIP());
-
 }
 
-int x    = Matrix.width();
-int pass = 0;
-
 void loop() {
-
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -85,7 +76,7 @@ void loop() {
   }
   
   // Wait until the client sends some data
-  Serial.println("new client");
+  Serial.println("New HTTP client connection");
   while(!client.available()){
     delay(1);
   }
@@ -94,42 +85,41 @@ void loop() {
   String command1 = client.readStringUntil('/');
   String command = client.readStringUntil('/');
   Serial.println(command);
-if (command == "text") {
-value = client.readStringUntil('/');
-z=1;
-value.replace("%20", " ");
-pixelsInText = (value.length() * 7)+8;
-  Serial.println(value);
-//value.toCharArray(charBuf, 70) ;
-}
-else {  // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-  s += file1;  
-  client.flush();
 
-  // Send the response to the client
-while(s.length()>2000)
-{
-  String dummy = s.substring(0,2000);
-  client.print(dummy);
-  s.replace(dummy," ");
-}
-  
-  client.print(s);
-  delay(1);
-  Serial.println("Client disconnected");
-
-  // The client will actually be disconnected 
-  // when the function returns and 'client' object is destroyed
-}
-if(z){
-  while(x+17 > (Matrix.width() - pixelsInText)){
- Matrix.fillScreen(0);
-  Matrix.setCursor(--x, 0);
-  Matrix.print(value);
-  Matrix.show();
-  delay(100);
+  if (command == "text") {
+    value = client.readStringUntil('/');
+    z=1;
+    value.replace("%20", " ");
+    pixelsInText = (value.length() * 7) + 8;
+    Serial.println(value);
   }
-  x    = Matrix.width();
-}
+  else { // Prepare the response
+    String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    s += page_html;  
+    client.flush();
+
+    // Send the response to the client
+    while(s.length() > 2000) {
+      String dummy = s.substring(0, 2000);
+      client.print(dummy);
+      s.replace(dummy, " ");
+    }
+  
+    client.print(s);
+    delay(1);
+    Serial.println("HTTP client disconnected");
+
+    // The client will actually be disconnected 
+    // when the function returns and 'client' object is destroyed
+  }
+  if(z) {
+    while(x + 17 > (Matrix.width() - pixelsInText)) {
+      Matrix.fillScreen(0);
+      Matrix.setCursor(--x, 0);
+      Matrix.print(value);
+      Matrix.show();
+      delay(100);
+    }
+    x = Matrix.width();
+  }
 }
