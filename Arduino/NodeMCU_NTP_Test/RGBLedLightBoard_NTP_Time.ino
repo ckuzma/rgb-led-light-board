8a7198adda8422012c0d883cd164d6bf37e75ff3
ckuzma <5973601+ckuzma@users.noreply.github.com>
2020-06-18 14:55:38 -0700

    Cleaned up code a lot, let's try synchronizing again

--- Arduino/NodeMCU_NTP_Test/RGBLedLightBoard_NTP_Time.ino
+++ Arduino/NodeMCU_NTP_Test/RGBLedLightBoard_NTP_Time.ino
@@ -41,8 +41,7 @@ const uint16_t BG_COLOR   = Matrix.Color(255, 0, 0); // full red
 IPAddress timeServerIP; // time.nist.gov NTP server address
 WiFiUDP udp;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println();
@@ -52,7 +51,7 @@ void setup()
   Serial.println(ssid);
   WiFi.begin(ssid, pass);
   
-  while (WiFi.status() != WL_CONNECTED) {
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -72,56 +71,42 @@ void setup()
   Matrix.setBrightness(255);
 }
 
-void loop()
-{
+void loop() {
   // Place to save epoch
   unsigned long epoch;
   
-  //get a random server from the pool
+  // Get a random server from the pool
   WiFi.hostByName(ntpServerName, timeServerIP); 
 
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  sendNTPpacket(timeServerIP); // Send an NTP packet to a time server
   // wait to see if a reply is available
   delay(1000);
   
+  // Read in the UDP buffer
   int cb = udp.parsePacket();
+
+  // If we have a UDP packet, do something
   if (!cb) {
-    Serial.println("no packet yet");
+    Serial.println("No UDP packet yet");
   }
   else {
-    Serial.print("packet received, length=");
+    // Print the packet length
+    Serial.print("UDP packet received, length=");
     Serial.println(cb);
-    // We've received a packet, read the data from it
+
+    // Read the received UDP packet
     udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
 
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
+    // Parse the packet and extrach the epoch
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    //Serial.print("Seconds since Jan 1 1900 = " );
-    //Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    //Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
     const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    //unsigned long epoch = secsSince1900 - seventyYears;
     epoch = secsSince1900 - seventyYears;
     epoch = epoch + (hoursAdjust * 3600);
-    // print Unix time:
-    //Serial.println(epoch);
-
-    // Print it
-    //printTime(epoch);
-
   }
-  // wait ten seconds before asking for the time again
-  //delay(10000);
+
+  // Print the time for 60 seconds
   printTimeForXSeconds(epoch, 60);
 }
 
@@ -182,26 +167,17 @@ void printTimeForXSeconds(long epoch, int seconds) {
   }
 }
 
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
+unsigned long sendNTPpacket(IPAddress& address) {
+  Serial.println("Sending an NTP packet request...");
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
   packetBuffer[1] = 0;     // Stratum, or type of clock
   packetBuffer[2] = 6;     // Polling Interval
   packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
   udp.beginPacket(address, 123); //NTP requests are to port 123
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   udp.endPacket();
