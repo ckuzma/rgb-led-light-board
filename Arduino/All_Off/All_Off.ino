ea121c27b4513dbb9e2a4c7cf283489e18da240a
Christopher Kuzma <5973601+ckuzma@users.noreply.github.com>
2023-04-11 18:26:29 -0400

    Added All_Off to enable wireless OTA control

---
+++ Arduino/All_Off/All_Off.ino
@@ -0,0 +1,80 @@
+// Arduino OTA stuff
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#ifndef STASSID
+#define STASSID "...."
+#define STAPSK "...."
+#endif
+const char* ssid = STASSID;
+const char* password = STAPSK;
+const char* host = "OTA-RGB-LED-LightBoard";
+
+// NeoMatrix stuff
+#include <Adafruit_NeoMatrix.h>
+
+// Change these to fit your setup
+#define PIN D3
+#define WIDTH 16
+#define HEIGHT 8
+const int BRIGHTNESS = 0;  // 0 = off, 255 = full brightness
+
+int displayArray[HEIGHT][WIDTH][3] = {
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+  {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+};
+
+// Initialize the matrix
+Adafruit_NeoMatrix Matrix = Adafruit_NeoMatrix(
+  WIDTH, HEIGHT,
+  PIN,
+  NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_ROWS   + NEO_MATRIX_ZIGZAG,
+  NEO_GRB           + NEO_KHZ800
+  );
+  
+void setup(){
+  // Get WiFi up and going for OTA purposes
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    WiFi.begin(ssid, password);
+    Serial.println("Retrying connection...");
+  }
+  ArduinoOTA.setHostname(host);
+  ArduinoOTA.begin();
+
+  // Initialize the matrix
+  Matrix.begin();
+  Matrix.setBrightness(BRIGHTNESS);
+
+  // Draw all of it to the Matrix
+  for(int y=0; y<HEIGHT; y++){
+    for(int x=0; x<WIDTH; x++){
+      Matrix.drawPixel(x, y, Matrix.Color(
+        displayArray[y][x][0], // Red
+        displayArray[y][x][1], // Green
+        displayArray[y][x][2]  // Blue
+      ));
+    }
+  }
+
+  // Show it
+  Matrix.show();
+}
+
+
+
+void loop(){
+  ArduinoOTA.handle();
+  // Nothing here
+}
